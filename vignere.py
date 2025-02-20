3f667bd57dd273f0cc4b180a6117b7882830792d
rakshith-411 <rakshithmurugendra@gmail.com>
2025-02-21 01:03:33 +0530

    Add files via upload

---
+++ vignere.py
@@ -0,0 +1,21 @@
+
+def vignere(key,message):
+message = message.lower()
+message = message.replace(' ','')
+m = len(key)
+cipherText = ""
+for i in range(len(message)):
+letter = message[i]
+k = key[i%m]
+cipherText+=chr((ord(letter)+k-97)%26+97)
+return cipherText
+
+key = input("Enter a keystream : ")
+key = [ord(letter) - 97 for letter in key]
+message = input("Enter a message : ")
+cipherText = vignere(key,message)
+print("CipherText : ",cipherText)
+print("Decrypting ")
+key = [-1*k for k in key]
+plainText = vignere(key,cipherText)
+print("Plain text : ",plainText)
